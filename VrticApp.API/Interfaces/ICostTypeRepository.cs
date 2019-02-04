9b0d797f5de035ace3bd2e773e2cf536b7ad50b9
sandi.mrgan <sandi.mrgan@live.com>
2019-02-04 22:17:02 +0100

    cost type

--- VrticApp.API/Interfaces/ICostTypeRepository.cs
+++ VrticApp.API/Interfaces/ICostTypeRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VrticApp.API.DTOs.CostType;
 using VrticApp.API.Models;
 
 namespace VrticApp.API.Interfaces
@@ -9,8 +10,8 @@ namespace VrticApp.API.Interfaces
     public interface ICostTypeRepository
     {
         Task<List<CostType>> Get();
-        Task<List<CostType>> Get(int id);
-        Task<CostType> Add(CostType costType);
-        Task<CostType> Update(int id, CostType costType);
+        Task<CostType> Get(int id);
+        Task<CostType> Add(CostTypeCreateDTO costType);
+        Task<CostType> Update(int id, CostTypeUpdateDTO costType);
     }
 }
--- VrticApp.API/Repositories/CostTypeRepository.cs
+++ VrticApp.API/Repositories/CostTypeRepository.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VrticApp.API.Contexts;
+using VrticApp.API.DTOs.CostType;
 using VrticApp.API.Interfaces;
 using VrticApp.API.Models;
 
@@ -9,24 +12,50 @@ namespace VrticApp.API.Repositories
 {
     public class CostTypeRepository : ICostTypeRepository
     {
-        public Task<CostType> Add(CostType costType)
+        private readonly MyContext _context;
+        public CostTypeRepository(MyContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+      
+
+        public async Task<List<CostType>> Get()
+        {
+            return await _context.CostTypes.AnyAsync() ? await _context.CostTypes.ToListAsync() : null;      
         }
 
-        public Task<List<CostType>> Get()
+        public async Task<CostType> Get(int id)
         {
-            throw new NotImplementedException();
+            return await _context.CostTypes.AnyAsync() ?
+                    await _context.CostTypes.Where(x => x.CostTypeId == id).FirstOrDefaultAsync() : null;
         }
 
-        public Task<List<CostType>> Get(int id)
+        public async Task<CostType> Add(CostTypeCreateDTO costType)
         {
-            throw new NotImplementedException();
+            CostType cType = new CostType
+            {
+                Name = costType.Name,
+                Description = costType.Description,
+                IsActive = costType.IsActive
+            };
+            await _context.CostTypes.AddAsync(cType);
+            await _context.SaveChangesAsync();
+            return await Get(cType.CostTypeId);
         }
 
-        public Task<CostType> Update(int id, CostType costType)
+        public async Task<CostType> Update(int id, CostTypeUpdateDTO costType)
         {
-            throw new NotImplementedException();
+            CostType cType = await _context.CostTypes.Where(x => x.CostTypeId == id).FirstOrDefaultAsync();
+            if (cType == null)
+                return null;
+            cType.Name = costType.Name;
+            cType.Description = costType.Description;
+            cType.IsActive = costType.IsActive;
+
+            await _context.SaveChangesAsync();
+            return await Get(id);
         }
+
+     
     }
 }
